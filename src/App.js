c45d369affdafd7650b25e8005742423cec8af06
HB <hbianan@gmail.com>
2019-11-19 15:25:50 -0500

    files updated

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import axios from 'axios'
+
+class App extends Component {
+      constructor(props){
+            super(props);
+            this.state = {
+              username: '',
+              userData: []
+            }
+        
+            this.handleChange = this.handleChange.bind(this)
+            this.handSubmit = this.handleSubmit.bind(this)
+      }
+
+      handleChange = e => {
+        this.setState({
+          username: e.target.value
+        })
+      }
+    
+      handleSubmit = e => {
+        e.preventDefault();
+
+        axios.get(`https://api.github.com/users/${this.state.username}`)
+        .then( result => {
+          
+          let info = result.data;
+          console.log(result.data)
+          this.setState({
+            userData: [
+              {
+                user: info.login,
+                userId: info.id,
+                photo: info.avatar_url,
+                bio: info.bio,
+                followers: info.followers
+              }
+            ]
+          })
+
+          // let kk = this.state.userData.map( k => {
+          //   return k
+          // })
+          
+          // return kk;
+
+        }).catch( error => console.log( error ))
+      }
+  render() {
+
+    let kk = this.state.userData.map( k => {
+      console.log(k)
+      return (
+        <div key={k.userId}>
+          <h1>{k.user}</h1>
+          <p>{k.userId}</p>
+          <img src={k.photo}/>
+          <p>{k.bio}</p>
+          <p>{k.followers}</p>
+        </div>
+      )
+    })
+
+    return (
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" onChange={this.handleChange} value={this.state.username} />
+          <button type="submit">Submit</button>
+        </form>
+      {kk}
+      </div>
+    )
+  }
 }
 
 export default App;
